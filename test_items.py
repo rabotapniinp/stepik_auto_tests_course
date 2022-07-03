c861541ec5ae3f98ee4b581262876b1a3ed8aecb
rabotapniinp <108588625+rabotapniinp@users.noreply.github.com>
2022-07-04 01:32:27 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,14 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-class TestMainPage1():
-    def test_button(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-        browser.get(link)
-        time.sleep(30)
-        but = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
-        try:
-            assert len(but.text) != 0, "the button is missing"
-        finally:
-            time.sleep(10)
-
+def test_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+    but = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert len(but.text) != 0, "the button is missing"
